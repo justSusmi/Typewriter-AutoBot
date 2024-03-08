ee2125120e14333bcff019c27627b334726fbc42
justSusmi <95434248+justSusmi@users.noreply.github.com>
2024-03-08 20:44:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -145,11 +145,11 @@ class Typewriter:
         
         for x in range(int(self.units)):
             
-            driver.clickElement('/html/body/div[5]/div[3]/div[2]/div[1]/a/div[3]')
+            driver.clickElement(UNIT)
         
             self.betterPrint("Clicking unit button. . .")
                 
-            driver.clickElement('/html/body/div[9]/div[3]/div/button')
+            driver.clickElement(START)
                 
             self.betterPrint("Clicking start button. . .")
                 
